d1f0c961b5d49179380e82ec05228e2ed6e9d1f4
targetlakshya <thetarget018@gmail.com>
2024-03-14 18:17:36 +0530

    NAV Bar in progress

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {links} from '../data';
+import "./navbar.css"
+
+function Navbar() {
+    return ( 
+        <nav className="nav">
+            <div className="nav__menu">
+                <ul className="nav__list">
+                    {links.map(({name,icon,path}, index) => {
+                        return(
+                            <li className="nav__item" key={index}>
+                                <NavLink to={path} className='nav__link'>
+                                    {icon}
+                                    <h3 className="nav__name">{name}</h3>
+                                </NavLink>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+
+            <div className="nav__toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
